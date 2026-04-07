6d72118e862a6fc9d014e6df23c6934d3d80c53d
richa-kaplesh <kapleshricha@gmail.com>
2026-04-07 15:23:17 +0530

    update

--- vector_store.py
+++ vector_store.py
@@ -6,7 +6,7 @@ def cosine_similarity(query_vec:np.ndarray,chunk_vecs:np.ndarray) ->np.ndarray:
     chunk_magnitudes = np.linalg.norm(chunk_vecs, axis=1)
     return  dot_product / (query_magnitude * chunk_magnitudes )
 
-def get_top_k_chunks(query_vec:np.ndarray, chunk_vecs:np.ndarray , k:int=3)->list[int]:
+def get_top_k(query_vec:np.ndarray, chunk_vecs:np.ndarray , k:int=3)->list[int]:
     similarities = cosine_similarity(query_vec, chunk_vecs)
     top_k_indices = np.argsort(similarities)[::-1][:k]
     return top_k_indices.tolist()
